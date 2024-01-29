76daaf4d414cd9676e8c9b64e10fd8df376dcb9a
kedar ling <kedarkamaf@gmail.com>
2024-01-29 06:18:23 +0530

    updated alert page

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,48 +1,59 @@
-import React from 'react';
-import '../../index.css';
+import React, { useState } from 'react';
+import './Sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faExclamationTriangle, faFilter, faBell, faUser, faCog, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+
 function Sidebar() {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [activePage, setActivePage] = useState(location.pathname);
+
+  const handleNavigation = (path) => {
+    setActivePage(path);
+    navigate(path);
+  };
+
+  const isPageActive = (path) => path === activePage;
+
   return (
     <nav className="nav">
-      <div className="nav-upper-options"> 
-        <div className="nav-option option1" onClick={() => navigate('/dashboard')}> 
-          <FontAwesomeIcon icon={faHome} className="nav-img" alt="dashboard" /> 
-          <h3> Dashboard</h3> 
-        </div> 
+      <div className="nav-upper-options">
+        <div className={`nav-option option1 ${isPageActive('/dashboard') && 'active'}`} onClick={() => handleNavigation('/dashboard')}>
+          <FontAwesomeIcon icon={faHome} className="nav-img" alt="dashboard" />
+          <h3> Dashboard</h3>
+        </div>
 
-        <div className="option2 nav-option"> 
-          <FontAwesomeIcon icon={faExclamationTriangle} className="nav-img" alt="threats" /> 
-          <h3> Threats</h3> 
-        </div> 
+        <div className={`nav-option option2 ${isPageActive('/threats') && 'active'}`} onClick={() => handleNavigation('/threats')}>
+          <FontAwesomeIcon icon={faExclamationTriangle} className="nav-img" alt="threats" />
+          <h3> Threats</h3>
+        </div>
 
-        <div className="nav-option option3"> 
-          <FontAwesomeIcon icon={faFilter} className="nav-img" alt="filter" /> 
-          <h3> Filter Logs</h3> 
-        </div> 
+        <div className={`nav-option option3 ${isPageActive('/filter-logs') && 'active'}`} onClick={() => handleNavigation('/filter-logs')}>
+          <FontAwesomeIcon icon={faFilter} className="nav-img" alt="filter" />
+          <h3> Filter Logs</h3>
+        </div>
 
-        <div className="nav-option option4"  onClick={() => navigate('/alerts')}> 
-          <FontAwesomeIcon icon={faBell} className="nav-img" alt="alerts" /> 
-          <h3> Alerts</h3> 
-        </div> 
+        <div className={`nav-option option4 ${isPageActive('/alerts') && 'active'}`} onClick={() => handleNavigation('/alerts')}>
+          <FontAwesomeIcon icon={faBell} className="nav-img" alt="alerts" />
+          <h3> Alerts</h3>
+        </div>
 
-        <div className="nav-option option5"> 
-          <FontAwesomeIcon icon={faUser} className="nav-img" alt="blog" /> 
-          <h3> Profile</h3> 
-        </div> 
+        <div className={`nav-option option5 ${isPageActive('/profile') && 'active'}`}>
+          <FontAwesomeIcon icon={faUser} className="nav-img" alt="blog" />
+          <h3> Profile</h3>
+        </div>
 
-        <div className="nav-option option6"> 
-          <FontAwesomeIcon icon={faCog} className="nav-img" alt="settings" /> 
-          <h3> Settings</h3> 
-        </div> 
+        <div className={`nav-option option6 ${isPageActive('/settings') && 'active'}`}>
+          <FontAwesomeIcon icon={faCog} className="nav-img" alt="settings" />
+          <h3> Settings</h3>
+        </div>
 
-        <div className="nav-option logout"> 
-          <FontAwesomeIcon icon={faSignOutAlt} className="nav-img" alt="logout" /> 
-          <h3> Logout</h3> 
-        </div> 
-      </div> 
+        <div className="nav-option logout">
+          <FontAwesomeIcon icon={faSignOutAlt} className="nav-img" alt="logout" />
+          <h3> Logout</h3>
+        </div>
+      </div>
     </nav>
   );
 }
