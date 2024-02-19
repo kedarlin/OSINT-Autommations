714056c3cfd69689c42e7a6593cf43237cfc5401
kedarlin <kedarkamaf@gmail.com>
2024-02-19 17:04:53 +0530

    updated header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import '../../index.css';
+import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faBell, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faBell, faUser, faWandSparkles, faHandSparkles, faMagnifyingGlass, faMagnifyingGlassPlus, faAssistiveListeningSystems, faGear, faCropSimple, faFileCircleCheck } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
     <header>
-      <div className="logosec">
-        <div className="logo">CyBros</div> 
+      <div className="navbar-logosec">
+        <div className="navbar-logo">CyBros</div>
       </div>
-      <div className="searchbar">
-        <input type="text" placeholder="Search" /> 
-        <div className="searchbtn"> 
-          <FontAwesomeIcon icon={faSearch} className="icn srchicn" alt="search-icon" style={{ color: 'white' }}/>
-        </div> 
+      <div className="navbar-items-container">
+        <div className='navbar-item'>
+          <div className="navbar-item-icon">
+            <FontAwesomeIcon icon={faMagnifyingGlassPlus} className="icn " alt="search-icon" style={{ color: 'gray' }} />
+          </div>
+          <div className="navbar-item-text">
+            New Scan  
+          </div>
+        </div>
+        <div className='navbar-item'>
+          <div className="navbar-item-icon">
+            <FontAwesomeIcon icon={faFileCircleCheck} className="icn " alt="search-icon" style={{ color: 'gray' }} />
+          </div>
+          <div className="navbar-item-text">
+            Scanned
+          </div>
+        </div>
+        <div className='navbar-item'>
+          <div className="navbar-item-icon">
+            <FontAwesomeIcon icon={faGear} className="icn " alt="search-icon" style={{ color: 'gray' }} />
+          </div>
+          <div className="navbar-item-text">
+            Settings
+          </div>
+        </div>
       </div>
       <div className="message">
-        <div className="circle"></div> 
-        <div className="dp"> 
-          <FontAwesomeIcon icon={faBell} className="dpicn" alt="notification" /> 
-          <FontAwesomeIcon icon={faUser} className="dpicn" alt="user-profile" /> 
-        </div> 
+        <div className="circle"></div>
+        <div className="dp">
+          <FontAwesomeIcon icon={faBell} className="dpicn" alt="notification" />
+          <FontAwesomeIcon icon={faUser} className="dpicn" alt="user-profile" />
+        </div>
       </div>
     </header>
   );
